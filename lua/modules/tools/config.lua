82c7b8e3edf20526c53d7a8f64ad2f6a3810b9db
wax5798 <810905061@qq.com>
2026-08-18 22:41:01 +0800

    修改telescope搜索过滤条件：不过滤vendor和build目录

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -52,7 +52,7 @@ function config.telescope()
             borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
             layout_strategy = "horizontal",
             path_display = { "smart" },
-            file_ignore_patterns = { ".git/", ".vscode/", ".cache", "%.class", "%.pdf", "%.mkv", "%.mp4", "%.zip", "tags", "node_modules", "target", "build", "dist", "vendor", "__pycache__" },
+            file_ignore_patterns = { ".git/", ".vscode/", ".cache", "%.class", "%.pdf", "%.mkv", "%.mp4", "%.zip", "tags", "node_modules", "target", "dist", "__pycache__" },
             vimgrep_arguments = {
                 "rg", "--color=never", "--no-heading",
                 "--with-filename", "--line-number", "--column", "--smart-case",
@@ -62,9 +62,7 @@ function config.telescope()
                 "--glob", "!.cache",
                 "--glob", "!node_modules",
                 "--glob", "!target",
-                "--glob", "!build",
                 "--glob", "!dist",
-                "--glob", "!vendor",
                 "--glob", "!__pycache__",
                 "--glob", "!tags",
                 "--glob", "!*.class",
